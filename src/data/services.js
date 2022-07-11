15c801c0dd57987f31badf737d81e6f4076ddbbd
Dhruvil <pdhruvil1908@gmail.com>
2022-07-11 20:52:47 +0530

    Images Added

--- src/data/services.js
+++ src/data/services.js
@@ -1,7 +1,7 @@
 const services = [
     {
         key: 1,
-        icon: "https://github.com/Dhruvil-P/Exigotech-Site/src/assets/Services/CloudServices.svg",
+        icon: "https://github.com/Dhruvil-P/Exigotech-Site/blob/main/src/assets/Services/CloudServices.svg",
         title: "Cloud Services",
         description: "Modernise your legacy infrastructure and distribute workloads smartly across the cloud platforms and benefit from digital business solutions.", 
     },
