f75b116c5b6d4d74a7606d30059888f1b5273a6a
Dhruvil <pdhruvil1908@gmail.com>
2022-07-11 20:58:06 +0530

    Images Added

--- src/data/services.js
+++ src/data/services.js
@@ -1,31 +1,31 @@
 const services = [
     {
         key: 1,
-        icon: "https://github.com/Dhruvil-P/Exigotech-Site/blob/main/src/assets/Services/CloudServices.svg",
+        icon: "src/assets/Services/CloudServices.svg",
         title: "Cloud Services",
         description: "Modernise your legacy infrastructure and distribute workloads smartly across the cloud platforms and benefit from digital business solutions.", 
     },
     {
         key: 2,
-        icon: "https://github.com/Dhruvil-P/Exigotech-Site/src/assets/Services/Infra.svg",
+        icon: "src/assets/Services/Infra.svg",
         title: "Infrastructure",
         description: "Build a robust IT infrastructure network for your employees and equip them with the latest solutions to perform better.", 
     },
     {
         key: 3,
-        icon: "https://github.com/Dhruvil-P/Exigotech-Site/src/assets/Services/Networks.svg",
+        icon: "src/assets/Services/Networks.svg",
         title: "Networks and Internet",
         description: "Drive core business activities with fully secured and reliable network and internet services.", 
     },
     {
         key: 4,
-        icon: "https://github.com/Dhruvil-P/Exigotech-Site/src/assets/Services/ITSupport.svg",
+        icon: "src/assets/Services/ITSupport.svg",
         title: "IT Support",
         description: "Bring your dispersed teams onto a common platform and empower them to collaborate and innovate.", 
     },
     {
         key: 5,
-        icon: "https://github.com/Dhruvil-P/Exigotech-Site/src/assets/Services/AppDev.svg",
+        icon: "src/assets/Services/AppDev.svg",
         title: "Application Development",
         description: "Harness the power of the latest technologies and software and climb the ladder of success by revolutionising your existing processes.", 
     },
