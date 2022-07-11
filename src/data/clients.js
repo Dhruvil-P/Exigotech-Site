a8aaab939e06a11709a667b1a9b6cdeb23850153
Dhruvil <pdhruvil1908@gmail.com>
2022-07-11 20:45:14 +0530

    Images Added

--- src/data/clients.js
+++ src/data/clients.js
@@ -2,21 +2,21 @@ const clients = [
     {
         key: 1,
         text: "Our association with Exigo Tech enabled us to automate our manual HR processes and heighten our productivity and efficiency levels.",
-        img: "/src/assets/Clients/Client1.jpg",
+        img: "https://exigotech.netlify.app/src/assets/Clients/Client1.jpg",
         name: "Mira Schleifer",
         rating: 4.8
     },
     {
         key: 2,
         text: "Exigo Tech created a highly complicated logistics platform for us, that enabled us to manage the entire logistics process properly.",
-        img: "/src/assets/Clients/Client2.jpg",
+        img: "https://exigotech.netlify.app/src/assets/Clients/Client2.jpg",
         name: "Zain Curtis",
         rating: 4.5
     },
     {
         key: 3,
         text: "Rhino Rack has gained a trusted advisor in Exigo Tech to advise and assist us with all of our business requirements.",
-        img: "/src/assets/Clients/Client3.jpg",
+        img: "https://exigotech.netlify.app/src/assets/Clients/Client3.jpg",
         name: "Charlie Rhiel Madsen",
         rating: 4.9
     },
